df03752fa9f209c34d1a46d13107f0d704614d1b
LENOVO <LENOVO@LAPTOP-QI2A2CFL>
2021-07-09 19:40:50 -0500

    Ejercicio en clase

---
+++ src/Diagramas/de/flujo/Main3.java
@@ -0,0 +1,23 @@
+package Diagramas.de.flujo;
+
+public class Main3 {
+
+	public Main3() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// Imprimir la tabla del 7 multiplicado hasta el 12
+
+		System.out.println("=== TABLA DEL 7 ===");
+
+		for (int t = 0; t <= 12; t++) {
+
+			int R = 7 * t;
+
+			System.out.println("7 x " + t + " = " + R);
+		}
+
+	}
+
+}
