0fad77eb5de95935d3360f1b2d2b231901655963
LENOVO <LENOVO@LAPTOP-QI2A2CFL>
2021-07-21 18:49:40 -0500

    tipos de datoss

---
+++ src/Ejem/de/tipos/de/datos/MainTiposDeDatos.java
@@ -0,0 +1,24 @@
+package Ejem.de.tipos.de.datos;
+
+public class MainTiposDeDatos {
+
+	public MainTiposDeDatos() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+
+		// LOS TIPOS DE DATOS
+
+		short CantidadEstudiante = 1000;
+		byte entradaCircuito = 15;
+		int cantidadPersonasBarrio = 10000;
+		long cantidadDeAves = 1000000000;
+		double CalificacionSemestre = 15.56;
+		float Sueldo = 200.60f;
+		char InicialNombre = 'J';
+		boolean heterosexual = true;
+
+	}
+
+}
